dd0c314134a9a919cc8b8f40066985da03863183
LTeijeiro <leonardo.teijeiro@atg.com.uy>
2023-03-08 18:10:30 -0300

    este es el quinto commit

---
+++ prueba.cbl
@@ -0,0 +1,14 @@
+       identification division.
+       program-id. prgrama de prueba.
+       
+       environment division.
+       configuration section.
+       
+       data division.
+       working-storage section.
+       
+       procedure division.
+        
+        goback.
+       
+       end program prgrama de prueba.
